66b0894440621d192193144a769d5483666a4af8
zeba1211 <zebapatel1211@gmail.com>
2025-03-03 14:31:18 +0530

    add requirement.txt

--- companyapi/settings.py
+++ companyapi/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'companyapi.urls'
