ecd6a9462a14a39def85208365257db5a63888d1
zeba1211 <zebapatel1211@gmail.com>
2025-03-06 14:45:00 +0530

    feching db on admin/

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+# admin.py
+from django.contrib import admin
+from silk.models import Profile, SQLQuery, Request
+
+admin.site.register(Profile)
+admin.site.register(SQLQuery)
+admin.site.register(Request)
+
